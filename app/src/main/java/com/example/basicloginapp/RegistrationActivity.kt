d4ce53b5af59b5f5d768cae89cf3709dd6410434
Rahul Raval <rahul@mutualart.com>
2022-11-30 13:10:50 +0530

    "Login Needs To BE Done With SQLITE"

--- app/src/main/java/com/example/basicloginapp/RegistrationActivity.kt
+++ app/src/main/java/com/example/basicloginapp/RegistrationActivity.kt
@@ -16,16 +16,16 @@ import java.lang.Exception
 import java.util.regex.Pattern
 
 class RegistrationActivity : AppCompatActivity() {
-    var nameLayout: TextInputLayout? = null
-    var emailLayout: TextInputLayout? = null
-    var passwordLayout: TextInputLayout? = null
-    var retypepasswordLayout: TextInputLayout? = null
+    private var nameLayout: TextInputLayout? = null
+    private var emailLayout: TextInputLayout? = null
+    private var passwordLayout: TextInputLayout? = null
+    private var retypepasswordLayout: TextInputLayout? = null
 
 
-    var name: TextInputEditText? = null
-    var email: TextInputEditText? = null
-    var password: TextInputEditText? = null
-    var retypepassword: TextInputEditText? = null
+    private var name: TextInputEditText? = null
+    private var email: TextInputEditText? = null
+    private var password: TextInputEditText? = null
+    private var retypepassword: TextInputEditText? = null
 
 
     private var registerButton: AppCompatButton? = null
@@ -52,7 +52,11 @@ class RegistrationActivity : AppCompatActivity() {
 
         dbhandler = databaseHandler(applicationContext)
 
-//        passwordEditTextWatcher(password, retypepassword, passwordLayout!!, retypepasswordLayout!!)
+
+        editTextWatcher(name,nameLayout)
+        emailEditTextWatcher(email,emailLayout)
+        passwordEditTextWatcher(password, retypepassword, passwordLayout, retypepasswordLayout)
+
 
         registerButton!!.setOnClickListener {
             userName = name!!.text.toString()
@@ -60,50 +64,133 @@ class RegistrationActivity : AppCompatActivity() {
             userPassword = password!!.text.toString()
             userReTypePassword = retypepassword!!.text.toString()
 
+            if(
+                emailVerification(userEmail!!) &&
+                passwordVerification(userPassword!!)) {
+                try{
+                        userRegisteration(userName!!, userEmail!!, userPassword!!)
+                }
+                catch (e : Exception)
+                {
+                    Log.e("TAG", "onCreate: $e")
+                }
 
-
-            if (emailVerification(userEmail!!) && passwordVerification(
-                    userPassword!!
-                )
-            ) {
-                userRegisteration(userName!!, userEmail!!, userPassword!!)
                 val intent =
                     Intent(this@RegistrationActivity, LoginActivity::class.java)
                 intent.flags =
                     Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                 startActivity(intent)
             }
+            else
+            {
+                Toast.makeText(baseContext, "Please Fill Required Fields", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-//    private fun passwordEditTextWatcher(
-//        ediText: TextInputEditText?,
-//        editText2: TextInputEditText?,
-//        passwordLayout: TextInputLayout?,
-//        retypepasswordLayout: TextInputLayout?,
-//    ) {
-//        editTextLayout!!.error = null
-//
-//        ediText!!.doOnTextChanged { text: CharSequence?, i: Int, i1: Int, i2: Int ->
-//            run {
-//                editTextLayout.error = null
-//                editTextLayout.clearFocus()
-//                if (text!!.length < 8 && !passwordVerification(text.toString())) {
-//                    editTextLayout.error =
-//                        "Password must contain !@#$%^&*()?><: and 8 characters long"
-//                }
-//                if(this@RegistrationActivity.retypepassword)
-//            }
-//        }
-//
-//        ediText.doAfterTextChanged { editable: Editable? ->
-//            run {
-//                if (editable!!.isEmpty()) {
-//                    editTextLayout.error = "Required"
-//                }
-//            }
-//        }
-//    }
+    private fun emailEditTextWatcher(emailEditText: TextInputEditText?, emailLayout: TextInputLayout?) {
+        emailEditText!!.doOnTextChanged { text, _, _, _ ->
+            run{
+                if(!emailVerification(text.toString()))
+                    emailLayout!!.error = "Please Enter Valid Email"
+                if(text!!.isEmpty())
+                    emailLayout!!.error = "Email Required !"
+            }
+        }
+
+        emailEditText.doAfterTextChanged { editable: Editable? ->
+            run{
+                if(editable!!.isNotEmpty() && emailVerification(editable.toString()))
+                {
+                    emailLayout!!.error = null
+                    emailLayout.clearFocus()
+                }
+            }
+        }
+    }
+
+    private fun editTextWatcher(nameEditText: TextInputEditText?, nameLayout: TextInputLayout?) {
+        nameEditText!!.doOnTextChanged { text, _, _, _ ->
+            run{
+                if(text!!.isEmpty())
+                    nameLayout!!.error = "Name cannot be empty"
+            }
+        }
+        
+        nameEditText.doAfterTextChanged { editable: Editable? ->
+            run{
+                if(editable!!.isNotEmpty())
+                {
+                    nameLayout!!.error = null
+                    nameLayout.clearFocus()
+                }
+            }
+        }
+    }
+
+    private fun passwordEditTextWatcher(
+        passwordEditText : TextInputEditText?,
+        retypePasswordEditText : TextInputEditText?,
+        passwordLayout : TextInputLayout?,
+        retypepasswordLayout: TextInputLayout?)
+    {
+        passwordEditText!!.doOnTextChanged { text: CharSequence?, _: Int, _: Int, _: Int ->
+            run {
+                //password 8 char validation
+                if (text!!.length < 8)
+                    passwordLayout!!.error = "Password length must be greater than 8"
+                else
+                {
+                    passwordLayout!!.error = null
+                    passwordLayout.clearFocus()
+                }
+
+                //password RegEx validation
+                if (text.length >= 8 && !passwordVerification(text.toString()))
+                    passwordLayout.error = "Password must contains !@#$%^&*()?><:"
+
+            }
+        }
+
+        retypePasswordEditText!!.doOnTextChanged { text: CharSequence?, _: Int, _: Int, _: Int ->
+            run {
+                //retypepassword == password validation
+               if(text!!.trim() != passwordEditText.text!!.trim())
+                   retypepasswordLayout!!.error = "Password doesnot matched !"
+
+                //remove error after validation checked
+                if(
+                    text.isNotEmpty() && text.length > 8 &&
+                    passwordVerification(text.toString()) &&
+                    retypePasswordEditText.text!!.trim().toString() == passwordEditText.text!!.trim().toString()
+                ) {
+                    retypepasswordLayout!!.error = null
+                    retypepasswordLayout.clearFocus()
+                }
+            }
+        }
+
+        passwordEditText.doAfterTextChanged { editable: Editable? ->
+            run {
+
+                //empty edittext validation
+                if (editable!!.isEmpty()) passwordLayout!!.error = "Required"
+                //remove error after validation checked
+                if(editable.isNotEmpty() && editable.length > 8 && passwordVerification(editable.toString()))
+                    passwordLayout!!.error = null
+                    passwordLayout!!.clearFocus()
+
+            }
+        }
+        
+        retypePasswordEditText.doAfterTextChanged { editable: Editable? ->
+            run{
+                //empty edittext validation
+                if (editable!!.isEmpty()) retypepasswordLayout!!.error = "Required"
+
+            }
+        }
+    }
 
     private fun userRegisteration(userName: String, userEmail: String, userPassword: String) {
         try {
